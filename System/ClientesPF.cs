6e53ca90671fa0068944ccefb579ee157126b5ed
kauafragozo <kauafragozo18@gmail.com>
2023-07-31 21:29:38 -0300

    Projeto-Lista.cs

---
+++ System/ClientesPF.cs
@@ -0,0 +1,7 @@
+class ClientesPF
+{
+    public int id {get;set;}
+    public string? nome {get;set;}
+    public string? cpf {get; set;}
+    public string? endereço{get;set;}
+}
---
+++ System/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace projetolista;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ClientesPF cl1 = new ClientesPF();
+
+        cl1.id = 1;
+        Console.WriteLine("Insira o Nome");
+        cl1.nome = Console.ReadLine();
+        Console.WriteLine("Insira o CPF");
+        cl1.cpf = Console.ReadLine();
+        Console.WriteLine("Insira o Endereço");
+        cl1.endereço = Console.ReadLine();
+        
+
+        ClientesPF cl2 = new ClientesPF();
+        cl2.id = 2;
+        cl2.nome = "Pedro";
+        cl2.cpf = "123456789";
+        cl2.endereço = "Rua da Paz";
+
+        ClientesPF cl3 = new ClientesPF();
+        cl3.id= 3;
+        cl3.nome = "Liara";
+        cl3.cpf = "1020304050";
+        cl3.endereço = "Rua dos Amores";
+        
+        List<ClientesPF> lista = new List<ClientesPF>();
+
+        lista.Add(new ClientesPF());
+        lista.Add(cl1);
+        lista.Add(cl2);
+        lista.Add(cl3);
+
+        foreach(ClientesPF aux in lista){
+
+            System.Console.WriteLine("Cliente {0}", aux.nome);
+            System.Console.WriteLine("CPF: {0}", aux.cpf);
+            System.Console.WriteLine("Endereço: {0}", aux.endereço);
+        
+        }
+
+
+
+    }
+}
